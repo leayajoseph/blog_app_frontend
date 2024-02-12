6a8f4aac0e8ad4e70c74f803e3712842a8513c7f
leayajoseph <leaya231@gmail.com>
2024-02-12 10:47:21 +0530

    repository created

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(blog());
+}
+class blog extends StatelessWidget {
+  const blog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
